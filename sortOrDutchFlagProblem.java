abbd57ff75a6e87e10b10791509346c290e2d15f
sushmi2598 <160345561+sushmi2598@users.noreply.github.com>
2024-02-20 20:09:49 +0530

    Update sortOrDutchFlagProblem.java

--- sortOrDutchFlagProblem.java
+++ sortOrDutchFlagProblem.java
@@ -1,1 +1,34 @@
-s
+class Solution {
+    public void sortColors(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        int mid=0;
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                mid++;
+                low++;
+                
+            }
+            else if(nums[mid]==1)
+            mid++;
+            else
+            {
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high]=temp;
+                high--;
+            }
+            for(int i:nums)
+            System.out.print(i);
+              System.out.println("\n");
+        }
+
+        for(int i:nums)
+        System.out.println(i);
+    }
+}
