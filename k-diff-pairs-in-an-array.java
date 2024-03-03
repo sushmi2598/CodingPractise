2420289efb4702500fde87efb5565e8264210036
sushmi2598 <160345561+sushmi2598@users.noreply.github.com>
2024-03-03 21:17:50 +0530

    Update k-diff-pairs-in-an-array.java

--- k-diff-pairs-in-an-array.java
+++ k-diff-pairs-in-an-array.java
@@ -1,21 +1,59 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
      Arrays.sort(nums);
+     HashSet<Integer> hs=new HashSet<>();
+        
+        for(int n: nums)
+        {
+            hs.add(n);
+        }
+        int[] arr= new int[hs.size()];
+        int j=0;
+        for(int i:hs)
+        {
+            arr[j++]=i;
+        }
+     
+    if(k==0)
+    {
+       HashMap<Integer,Integer> hm = new HashMap<>();
+       for(int i:nums)
+       {
+           if(hm.containsKey(i))
+           {
+               hm.put(i,hm.get(i)+1);
+           }
+           else hm.put(i,1);
+       }
+
+       int count=0;
+
+       for(int i:arr)
+       {
+           if(hm.get(i)>1){count++; 
+           hm.put(i,0);}
+       }
+       return count;
+    }
+    else
+    {
+        return getCount(arr,k);
+    }
+  
+    }
+
+    public static int getCount(int[] nums,int k){
      int l=0;
      int r=1;
      int count=0;
-     if(k==0)return 1;
-    for(int n:nums) System.out.print(n);
-    System.out.println("");
     while(r<nums.length && l<nums.length)
-    { System.out.println(Math.abs(nums[l]-nums[r]));
-        if(Math.abs(nums[l]-nums[r]) == k){
+    {   System.out.println(Math.abs(nums[l]-nums[r]));
+        if(Math.abs(nums[l]-nums[r]) == k && l!=r){
         count++;
         l++;
         r++;}
-        else if(Math.abs(nums[l]-nums[r]) < k)r++;
-        else if(Math.abs(nums[l]-nums[r]) > k)l++;
-    
+        else if(Math.abs(nums[l]-nums[r]) > k) l++;
+        else  r++;
     }
     return count;
     }
